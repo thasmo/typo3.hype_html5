1a8610561151f76b2ad2bb2f8817ac483eef8c9b
Thomas Deinhamer <thasmo@gmail.com>
2011-07-30 13:31:30 +0000

    Remove unnecessary attributes from resource elements.

--- Classes/Hook/class.user_hypehtml5_t3lib_pagerenderer.php
+++ Classes/Hook/class.user_hypehtml5_t3lib_pagerenderer.php
@@ -23,14 +23,28 @@ class user_hypehtml5_t3lib_pagerenderer {
 	 */
 	public function renderPostProcess($parameters, $renderer) {
 
+		# get language token
 		$languageToken = ($language = $renderer->getLanguage())
 			? $language
 			: 'en';
 
+		# fix <html> opening tag
 		$parameters['htmlTag'] = '<!--[if lt IE 7]><html class="no-js ie6" lang="' . $languageToken . '"><![endif]-->
 <!--[if IE 7]>   <html class="no-js ie7" lang="' . $languageToken . '"><![endif]-->
 <!--[if IE 8]>   <html class="no-js ie8" lang="' . $languageToken . '"><![endif]-->
 <!--[if gt IE 8]><!--><html class="no-js" lang="' . $languageToken . '"><!--<![endif]-->';
+
+		# fix style tags
+		$parameters['cssFiles'] = str_replace(array(' type="text/css"', ' media="all"'), '', $parameters['cssFiles']);
+
+		# fix script tags
+		$parameters['jsLibs'] = str_replace(' type="text/javascript"', '', $parameters['jsLibs']);
+		$parameters['jsFiles'] = str_replace(' type="text/javascript"', '', $parameters['jsFiles']);
+		$parameters['jsFooterFiles'] = str_replace(' type="text/javascript"', '', $parameters['jsFooterFiles']);
+
+		# fix custom data
+		$parameters['headerData'] = str_replace(array(' type="text/javascript"', ' type="text/css"', ' media="all"'), '', $parameters['headerData']);
+		$parameters['footerData'] = str_replace(array(' type="text/javascript"', ' type="text/css"', ' media="all"'), '', $parameters['footerData']);
 	}
 }
 
