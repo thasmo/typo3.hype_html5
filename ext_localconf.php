d36d45cbe1aa1fefbfa8334cc1669ed923a147ea
Thomas Deinhamer <thasmo@gmail.com>
2011-08-02 23:41:36 +0000

    Crucial fix for rendering plugins.

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,6 +19,17 @@ if(TYPO3_MODE == 'FE') {
 
 	# HTML Parser
 	$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']['t3lib/class.t3lib_parsehtml.php'] = t3lib_extMgm::extPath('hype_html5', 'Classes/XClass/class.ux_t3lib_parsehtml.php');
+
+	# MAGIC
+	$path = 'hypehtml5/Configuration/TypoScript/CSC/';
+
+	if(is_array($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'])) {
+		array_push($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'], $path);
+	} else {
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array($path);
+	}
+
+	unset($path);
 }
 
 ?>
