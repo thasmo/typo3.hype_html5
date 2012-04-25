9a9ed6e2f5f73c426d175403f6e392db85a69906
Thomas Deinhamer <thasmo@gmail.com>
2012-04-25 23:48:57 +0000

    Fixes #36379.

--- ext_tables.php
+++ ext_tables.php
@@ -13,4 +13,29 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Hype HTML5')
 # Content Rendering
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/CSC/', 'CSS Styled Content, HTML5');
 
+
+
+# TABLES
+
+# Content
+$columns = array(
+	'tx_hypehtml5_link' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:hype_html5/Resources/Private/Language/locallang_db.xml:tt_content.tx_hypehtml5_link',
+		'config' => array(
+			'type' => 'check',
+			'items' => array(
+				'1'	=> array(
+					'0' => 'LLL:EXT:lang/locallang_core.xml:labels.enabled',
+				),
+			),
+		),
+	),
+);
+
+t3lib_div::loadTCA('tt_content');
+t3lib_extMgm::addTCAcolumns('tt_content', $columns, 1);
+t3lib_extMgm::addFieldsToPalette('tt_content', 'header', 'tx_hypehtml5_link', 'after:header_link');
+t3lib_extMgm::addFieldsToPalette('tt_content', 'headers', 'tx_hypehtml5_link', 'after:header_link');
+
 ?>
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content (
+	tx_hypehtml5_link tinyint(1) DEFAULT '0' NOT NULL
+);
