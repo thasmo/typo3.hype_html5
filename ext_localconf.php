7844facc9e1603a21377e3b5ebaeeb713357aa55
Thomas Deinhamer <thasmo@gmail.com>
2012-07-05 23:51:51 +0000

    Adds some more changes. :>

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,33 +7,34 @@ if(!defined('TYPO3_MODE'))
 
 # SETUP
 
-# Page TSConfig
-t3lib_extMgm::addPageTSConfig('
-	TCEFORM {
-		tt_content {
-			text_properties.disabled = 1
-			text_align.disabled = 1
-			text_color.disabled = 1
-			text_face.disabled = 1
-			text_size.disabled = 1
-			image_frames.disabled = 1
+if(!t3lib_extMgm::isLoaded('css_styled_content')) {
 
-			CType.removeItems = rte,script,splash,swfobject,qtobject,multimedia,search
-			imageorient.types.image.removeItems = 8,9,10,17,18,25,26
+	# Page TSConfig
+	t3lib_extMgm::addPageTSConfig('
+		TCEFORM {
+			tt_content {
+				text_properties.disabled = 1
+				text_align.disabled = 1
+				text_color.disabled = 1
+				text_face.disabled = 1
+				text_size.disabled = 1
+				image_frames.disabled = 1
+
+				CType.removeItems = rte,script,splash,swfobject,qtobject,multimedia,search
+				imageorient.types.image.removeItems = 8,9,10,17,18,25,26
+			}
 		}
-	}
-');
-
-# Content Rendering
-if(is_array($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'])) {
-	array_push($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'], 'hypehtml5/Configuration/TypoScript/Frontend/');
-} else {
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array(
-		'hypehtml5/Configuration/TypoScript/Frontend/',
-	);
+	');
 }
 
-# TYPO3 4.5
+# Content Rendering
+if(!is_array($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'] = array();
+}
+
+array_push($GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'], 'hypehtml5/Configuration/TypoScript/Frontend/');
+
+# TYPO3 < 4.6
 if(t3lib_div::int_from_ver(TYPO3_version) < 4006000) {
 
 	# Login
@@ -53,7 +54,7 @@ if(t3lib_div::int_from_ver(TYPO3_version) < 4006000) {
 if(TYPO3_MODE == 'FE') {
 
 	# TSLib Frontend
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']['tslib/class.tslib_fe.php'] = t3lib_extMgm::extPath('hype_html5', 'Classes/XClass/class.ux_tslib_fe.php');
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']['tslib/class.tslib_fe.php'] = t3lib_extMgm::extPath($_EXTKEY, 'Classes/XClass/class.ux_tslib_fe.php');
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Hook/class.user_hypehtml5_tslib_fe.php:user_hypehtml5_tslib_fe->configArrayPostProc';
 
 	# Page Renderer
@@ -61,7 +62,7 @@ if(TYPO3_MODE == 'FE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Hook/class.user_hypehtml5_t3lib_pagerenderer.php:user_hypehtml5_t3lib_pagerenderer->renderPreProcess';
 
 	# HTML Parser
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']['t3lib/class.t3lib_parsehtml.php'] = t3lib_extMgm::extPath('hype_html5', 'Classes/XClass/class.ux_t3lib_parsehtml.php');
+	$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']['t3lib/class.t3lib_parsehtml.php'] = t3lib_extMgm::extPath($_EXTKEY, 'Classes/XClass/class.ux_t3lib_parsehtml.php');
 }
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -17,13 +17,16 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Frontend/', 'HTM
 
 # TABLES
 
-# Content
-t3lib_div::loadTCA('tt_content');
+if(!t3lib_extMgm::isLoaded('css_styled_content')) {
 
-t3lib_extMgm::addPiFlexFormValue('*', 'FILE:EXT:hype_html5/Configuration/FlexForm/Table.xml', 'table');
+	# Content
+	t3lib_div::loadTCA('tt_content');
 
-$TCA['tt_content']['types']['table']['showitem'] = 'CType;;4;;1-1-1, hidden, header;;3;;2-2-2, linkToTop;;;;4-4-4, --div--;LLL:EXT:cms/locallang_ttc.xml:CType.I.5, layout;;10;;3-3-3, cols, bodytext;;9;nowrap:wizards[table], text_properties, pi_flexform, --div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access, starttime, endtime, fe_group';
-$TCA['tt_content']['columns']['section_frame']['config']['items'][0] = array('LLL:EXT:hype_html5/Resources/Private/Language/locallang_db.xml:tt_content.tx_cssstyledcontent_section_frame.I.0', '0');
-$TCA['tt_content']['columns']['section_frame']['config']['items'][9] = array('LLL:EXT:hype_html5/Resources/Private/Language/locallang_db.xml:tt_content.tx_cssstyledcontent_section_frame.I.9', '66');
+	t3lib_extMgm::addPiFlexFormValue('*', 'FILE:EXT:hype_html5/Configuration/FlexForm/Table.xml', 'table');
+
+	$TCA['tt_content']['types']['table']['showitem'] = 'CType;;4;;1-1-1, hidden, header;;3;;2-2-2, linkToTop;;;;4-4-4, --div--;LLL:EXT:cms/locallang_ttc.xml:CType.I.5, layout;;10;;3-3-3, cols, bodytext;;9;nowrap:wizards[table], text_properties, pi_flexform, --div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access, starttime, endtime, fe_group';
+	$TCA['tt_content']['columns']['section_frame']['config']['items'][0] = array('LLL:EXT:hype_html5/Resources/Private/Language/locallang_db.xml:tt_content.tx_cssstyledcontent_section_frame.I.0', '0');
+	$TCA['tt_content']['columns']['section_frame']['config']['items'][9] = array('LLL:EXT:hype_html5/Resources/Private/Language/locallang_db.xml:tt_content.tx_cssstyledcontent_section_frame.I.9', '66');
+}
 
 ?>
